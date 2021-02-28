3995b968e790769807903861049af7afbac3d1bd
Maxim <maximrez@mail.ru>
2021-03-01 02:04:52 +0300

    commit

---
+++ cmake-build-debug/CMakeFiles/lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab.dir/Matrix.c.o"
+  "CMakeFiles/lab.dir/main.c.o"
+  "lab.exe"
+  "lab.pdb"
+  "liblab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
